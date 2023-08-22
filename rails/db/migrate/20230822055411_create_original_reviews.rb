1cd5226fe7a3dc0a89538b7c70f0b1f7d61d8293
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-22 07:03:00 +0000

    add 'null: false' to migration files

--- rails/db/migrate/20230822055411_create_original_reviews.rb
+++ rails/db/migrate/20230822055411_create_original_reviews.rb
@@ -5,31 +5,31 @@ class CreateOriginalReviews < ActiveRecord::Migration[7.0]
       t.integer :property_city_id, null: false, index: true
       # t.references :city, null: false, foreign_key: true
       t.references :store, null: false, foreign_key: true
-      t.string :address
-      t.string :property
-      t.integer :num_sale
-      t.date :date_considered
-      t.date :date_assessment
-      t.date :start_sale
-      t.date :end_sale
-      t.date :date_handover
-      t.integer :score_speed
-      t.integer :price_assessment
-      t.integer :price_sale
-      t.boolean :is_discount
-      t.integer :discount_n_month_later
-      t.integer :price_discount
-      t.integer :price_contract
-      t.integer :score_contract
-      t.integer :contract_type
-      t.string :headline
-      t.string :reason_sale
-      t.string :anxiety
-      t.string :reason_decision
-      t.integer :score_store
-      t.string :reason_score
-      t.string :advice
-      t.string :improvement
+      t.string :property_address, null:false
+      t.integer :property, null: false #物件種別
+      t.integer :num_sale, null: false #売却回数は選択肢
+      t.date :date_considered,null: false
+      t.date :date_assessment,null: false
+      t.date :start_sale,null: false
+      t.date :end_sale,null: false
+      t.date :date_handover,null: false
+      t.integer :score_speed,null: false
+      t.integer :price_assessment, null: false
+      t.integer :price_sale, null: false
+      t.boolean :is_discount, null: false
+      t.integer :discount_n_month_later #売り出してから何ヶ月後に値下げしたか null OK
+      t.integer :price_discount #null OK
+      t.integer :price_contract, null: false
+      t.integer :score_contract, null: false
+      t.integer :contract_type, null: false
+      t.string :headline, null: false
+      t.integer :reason_sale, null: false #売却理由は選択肢
+      t.string :anxiety, null: false
+      t.string :reason_decision, null: false
+      t.integer :score_store, null: false #対応満足度
+      t.string :reason_score, null: false
+      t.string :advice, null: false
+      t.string :improvement #null OK
 
       t.timestamps
     end
