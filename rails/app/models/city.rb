701cb51fb0e538a9834706d35e0fb36d3b7e11ad
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-23 07:32:58 +0000

    change city's deependences

--- rails/app/models/city.rb
+++ rails/app/models/city.rb
@@ -2,9 +2,9 @@
 
 class City < ApplicationRecord
   belongs_to :prefecture
-  has_many :assessment_users, dependent: :destroy
-  has_many :public_reviews, dependent: :destroy
-  has_many :original_reviews, dependent: :destroy
+  has_many :assessment_users, dependent: :nullify
+  has_many :public_reviews, dependent: :nullify
+  has_many :original_reviews, dependent: :nullify
   has_many :assessment_areas, dependent: :destroy
   has_many :stores, dependent: :destroy, through: :assessment_areas
 end
