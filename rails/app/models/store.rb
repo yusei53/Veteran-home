4ad77ee85c6a53e6c22008b56029e93bb9ae0f6d
joko <53999148+joko0811@users.noreply.github.com>
2023-08-23 05:29:13 +0000

    [fix] typo

--- rails/app/models/store.rb
+++ rails/app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ApplicationRecord
   has_one :company
   belongs_to :city
-  has_many :assessment_area, belongs_to: :destroy, through: :ciry
+  has_many :assessment_area, dependent: :destroy, through: :city
   has_many :original_review
   has_many :public_review
 end
