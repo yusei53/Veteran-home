faa3ab3d2b887e98aacb6c1b4bdd44778d39dfb8
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-23 09:43:43 +0000

    exec rubocop

--- rails/db/migrate/20230823122862_remove_prefcture_id_from_prefectures.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemovePrefctureIdFromPrefectures < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :prefectures, :prefcture_id, :integer
-  end
-end
