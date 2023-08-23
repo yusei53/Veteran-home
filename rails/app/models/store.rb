efd63b38de38626ac7ad3b314842022c4fedff90
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-23 07:39:52 +0000

    change city and store's dependences to assessment_area

--- rails/app/models/store.rb
+++ rails/app/models/store.rb
@@ -3,7 +3,7 @@
 class Store < ApplicationRecord
   belongs_to :company
   belongs_to :city
-  has_many :cities, dependent: :destroy, through: :assessment_areas
+  has_many :cities, dependent: :nullify, through: :assessment_areas
   has_many :assessment_areas, dependent: :destroy
   has_many :original_reviews, dependent: :destroy
 end
