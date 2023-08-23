30ccb7ed005bf6ffdf6ec4da547d12783915c41a
joko <53999148+joko0811@users.noreply.github.com>
2023-08-23 05:18:38 +0000

    [fix] typo

--- rails/app/models/assessment_user.rb
+++ rails/app/models/assessment_user.rb
@@ -1,5 +1,5 @@
 class AssessmentUser < ApplicationRecord
   belongs_to :city
   has_many :public_review, dependent: :destroy
-  has_many :original_review, dependent :destroy
+  has_many :original_review, dependent: :destroy
 end
