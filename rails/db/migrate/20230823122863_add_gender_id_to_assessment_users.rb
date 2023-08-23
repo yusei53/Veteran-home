282cbc2fe2087a5d6f9a2e311e407e64b71cb069
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-23 09:40:37 +0000

    exec rubocop

--- rails/db/migrate/20230823122863_add_gender_id_to_assessment_users.rb
+++ rails/db/migrate/20230823122863_add_gender_id_to_assessment_users.rb
@@ -2,6 +2,6 @@
 
 class AddGenderIdToAssessmentUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :assessment_users, :gender_id, :integer, :null, false
+    add_column :assessment_users, :gender_id, :integer
   end
 end
