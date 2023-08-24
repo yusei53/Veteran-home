98a5b047edcd24611aa7bf9cef76cc3f97ee14dc
yusei53 <yusei530922@gmail.com>
2023-08-24 06:14:03 +0000

    rubocopに修正してもらった

--- rails/app/controllers/stores_controller.rb
+++ rails/app/controllers/stores_controller.rb
@@ -2,9 +2,9 @@
 
 class StoresController < ApplicationController
   def show
-    @store = Store.find(ieul_store_id:params[:id])
+    @store = Store.find(ieul_store_id: params[:id])
     @company = @store.company
-    original_reviews=OriginalReview.where(store:@store)
-    @public_reviews = PublicReview.where(original_review:original_reviews)
+    original_reviews = OriginalReview.where(store: @store)
+    @public_reviews = PublicReview.where(original_review: original_reviews)
   end
 end
