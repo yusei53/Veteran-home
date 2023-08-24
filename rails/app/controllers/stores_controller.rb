d11ba5de922ceea82b2eb969e8cf6c4ef5ac9630
yusei53 <yusei530922@gmail.com>
2023-08-24 06:12:47 +0000

    口コミ詳細取得・表示

--- rails/app/controllers/stores_controller.rb
+++ rails/app/controllers/stores_controller.rb
@@ -2,7 +2,9 @@
 
 class StoresController < ApplicationController
   def show
-    @store = Store.find(params[:id])
+    @store = Store.find(ieul_store_id:params[:id])
     @company = @store.company
+    original_reviews=OriginalReview.where(store:@store)
+    @public_reviews = PublicReview.where(original_review:original_reviews)
   end
 end
