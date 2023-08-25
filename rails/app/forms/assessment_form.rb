9c39e7a259e3bf6bf619d6d977c54da01c7a1ae2
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-25 05:27:33 +0000

    tmp

--- rails/app/forms/assessment_form.rb
+++ rails/app/forms/assessment_form.rb
@@ -6,6 +6,11 @@ class AssessmentForm
     attribute :yomi, :string 
     attribute :phone_number, :string 
     attribute :email, :string
+
+    attribute :branch, :integer #査定依頼を出す店舗名
+    attribute :property_city, :integer #物件の市区
+    attribute :property_address, :string #物件の詳しい住所
+    attribute :property_type, :integer
     
 
 
---
+++ rails/app/models/building_area_unit.rb
@@ -0,0 +1,8 @@
+
+class BuildingAreaUnit < ActiveHash::Base
+    self.data = [
+      { id: 1, name: '平方メートル' },
+      { id: 2, name: '坪' }
+    ]
+  end
+  
