49b7abfc0ac6e455af4e5627a615422678e0ea3d
joko <53999148+joko0811@users.noreply.github.com>
2023-08-25 08:02:32 +0000

    t

--- rails/app/models/city.rb
+++ rails/app/models/city.rb
@@ -3,11 +3,11 @@
 class City < ApplicationRecord
   belongs_to :prefecture
   has_many :assessment_users, dependent: :nullify
-  # has_many :public_reviews, dependent: :nullify
   has_many :original_reviews, dependent: :nullify
-
   has_many :assessment_areas, dependent: :destroy
   has_many :stores, dependent: :nullify, through: :assessment_areas
 
-  validates :name, :yomi, :city_id, presence: true
+  validates :name, :yomi, presence: true
+  # 　city_idは Cityのキーではない、正しくはieul_city_id
+  validates :city_id, presence: true, numericality: { only_integer: true }
 end
