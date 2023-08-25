66f2f38e5df1359516aa44b51f6f328189c75bb6
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-25 05:28:22 +0000

    exec rubocop

--- rails/spec/requests/test_store_views_spec.rb
+++ rails/spec/requests/test_store_views_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "TestStoreViews", type: :request do
-  describe "GET /test_store_views" do
-    it "works! (now write some real specs)" do
-      store=FactoryBot.create(:store)
+RSpec.describe 'TestStoreViews' do
+  describe 'GET /test_store_views' do
+    it 'works! (now write some real specs)' do
+      store = create(:store)
       get store_path(store)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
