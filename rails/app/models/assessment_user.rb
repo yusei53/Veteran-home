711c1a2e80e2143a0009eefd382036920bc77f2b
joko <53999148+joko0811@users.noreply.github.com>
2023-08-25 02:50:04 +0000

    add validates

--- rails/app/models/assessment_user.rb
+++ rails/app/models/assessment_user.rb
@@ -2,13 +2,15 @@
 
 class AssessmentUser < ApplicationRecord
   has_one :original_review, dependent: :destroy
+  belongs_to :city
+  
+  # nameに対するバリデーション。イエウールAPIの仕様を満たす
+  # validates_with ShimeiFormatValidator
 
-  validates :name, :yomi, shimei_format:
+  validates_with EmailFormatValidator
 
-  # validate :email, email_format:
-  validates :name, :yomi, presence: true
+  validates :name, presence: true
   validates :email, presence: true
-  validates :is_recieved, presence: true
-  # validates :original_review_id, presence: true
+  validates :is_recieved, presence: true, inclusion: [true,false]
   validates :gender_id, inclusion: { in: Gender.all.map(&:id) }
 end
