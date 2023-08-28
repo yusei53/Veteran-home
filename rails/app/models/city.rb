75abb0d822ad6ec18111a50547dd7387308b5ce3
joko <53999148+joko0811@users.noreply.github.com>
2023-08-28 09:17:41 +0000

    fix association

--- rails/app/models/city.rb
+++ rails/app/models/city.rb
@@ -3,7 +3,6 @@
 class City < ApplicationRecord
   belongs_to :prefecture
   has_many :assessment_users, dependent: :nullify
-  has_many :public_reviews, dependent: :nullify
   has_many :original_reviews, dependent: :nullify
   has_many :assessment_areas, dependent: :destroy
   has_many :stores, dependent: :nullify, through: :assessment_areas
