ddf87290988ff212af09e3b62010601758e5a8a9
joko <53999148+joko0811@users.noreply.github.com>
2023-08-29 09:24:35 +0000

    rm コメント

--- rails/app/validators/email_validator.rb
+++ rails/app/validators/email_validator.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# class EmailValidator < ActiveModel::Validator
-#   def validate(record)
-#     # 名字＋半角スペース+名前の形式であるかを判定する
-#     return if record.email.nil?
-
-#     return if record.email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-#     record.errors.add((options[:message] || "氏名が不正なフォーマットです：#{record.email}"))
-#   end
-# end
-
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
     return if value.blank?
