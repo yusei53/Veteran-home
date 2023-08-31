f6aa69b029b49ff2f0679fbda6007009979500c8
takaya-hirano-su <132239631+takaya-hirano-su@users.noreply.github.com>
2023-08-31 02:59:36 +0000

    tmp

--- rails/app/javascript/controllers/hello_controller.js
+++ rails/app/javascript/controllers/hello_controller.js
@@ -7,31 +7,28 @@ export default class extends Controller {
     cities:Array,
     stores:Array,
     assessmentareas:Array,
-    companies:Array
   }
 
   connect(){
-    console.log(233333333)
-    console.log(this.storesValue)
-    console.log(this.assessmentareasValue)
-    console.log(this.companiesValue)
 
-    this.filter_city(1);
-    this.filter_store(1);
+    this.prefectureTarget.value=1
+    this.cityTarget.value=1
+    this.filter_city_and_store();
 
   }
 
-  greet() {
-    console.log(11111)
+  filter_city_and_store(){
+    this.filter_city();
+    this.filter_store();
   }
 
-  filter_city(prefecture_id){
+  filter_city(){
     this.cityTarget.innerHTML="";
 
-    var options=this.citiesValue.filter((city)=>{
-      return city.prefecture_id==prefecture_id;
+    let options=this.citiesValue.filter((city)=>{
+      return city.prefecture_id==this.prefectureTarget.value;
     }).map((city)=>{
-      var option=document.createElement("option");
+      let option=document.createElement("option");
       option.text=city.name;
       option.value=city.id;      
       return option
@@ -42,57 +39,35 @@ export default class extends Controller {
     });
   }
 
-  filter_store(city_id){
+  filter_store(){
     this.storeTarget.innerHTML="";
 
-    console.log("cityid")
-    console.log(city_id)
-
-    var assessment_store_ids=this.assessmentareasValue.filter((area)=>{
-      return area.city_id==city_id;
+    let assessment_store_ids=this.assessmentareasValue.filter((area)=>{
+      return area.city_id==this.cityTarget.value;
     }).map((area)=>{
       return area.store_id;
     })
-    console.log("store_ids--------");
-    console.log(assessment_store_ids);
 
-    var assessment_stores=this.storesValue.filter((store)=>{
+    let assessment_stores=this.storesValue.filter((store)=>{
       return assessment_store_ids.includes(store.id)
     })
-    console.log("stores--------");
-    console.log(assessment_stores);
 
-    var assessment_company_ids=this.storesValue.filter((store)=>{
-      return assessment_store_ids.includes(store.id)
-    }).map((store)=>{
-      return store.company_id;
-    })
-    console.log("comp_ids--------");
-    console.log(assessment_company_ids);
+    for(let i=0; i<assessment_stores.length;i++){
 
-    var assessment_companies=this.companiesValue.filter((company)=>{
-      return assessment_company_ids.includes(company.id);
-    })
-    console.log("comp--------");
-    console.log(assessment_companies);
-
-    for(var i=0; i<assessment_stores.length;i++){
+      let store_name="";
       if (assessment_stores[i].name === null ){
-        var name=assessment_companies[i].name;
+        store_name=assessment_stores[i].company_name;
       }else{
-        var name=assessment_companies[i].name+" "+assessment_stores[i].name;
+        store_name=assessment_stores[i].company_name+" "+assessment_stores[i].name;
       }
       
-      var option=document.createElement("option");
-      option.text=name;
+      let option=document.createElement("option");
+      option.text=store_name;
       option.value=assessment_stores[i].id;
       this.storeTarget.appendChild(option);
     }
-   
+
+    
   }
 
-  filter(){
-    this.filter_city(this.prefectureTarget.value);
-    this.filter_store(this.cityTarget.value);
-  }
 }
