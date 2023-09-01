e984b1f4aad8a7e63eb9509e183f7577a0106bd3
yusei53 <yusei530922@gmail.com>
2023-09-01 01:10:31 +0000

    metaタグ追加

--- rails/app/controllers/city_controller.rb
+++ rails/app/controllers/city_controller.rb
@@ -14,6 +14,12 @@ class CityController < ApplicationController
       latest_public_review = PublicReview.find_by(original_review: latest_original_review)
       @latest_public_reviews.push(latest_public_review)
     end
+
+    set_meta_tags(
+      title:"#{session[:assessment_city]["name"]}""の不動産売却体験談を公開中",
+      description: "#{session[:assessment_city]["name"]}" "の不動産売却に強い不動産会社の体験談を紹介します。不動産の価格査定が素早く・簡単にできるのはもちろん、弊社で審査して厳選した、「土地・（中古）マンション・（中古）一戸建て」の売却・買取・査定が得意な、地元のおすすめの加盟不動産会社を比較検討できます。「信頼できる不動産会社を見つけるのが難しい」「自分が知っている不動産会社を信頼してよいかわからない」という方はぜひご活用ください。",
+      keywords: "不動産売却,不動産査定,不動産会社,不動産仲介業者,不動産屋,住宅,住まい,マンション,一戸建て,土地,投資,家売る,ベテランすまい"
+    )
   end
 
   private
