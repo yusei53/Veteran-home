fb9cae94bfdb8f06980c1e7a164a46b21cef7646
yusei53 <yusei530922@gmail.com>
2023-09-01 01:15:13 +0000

    rubocop

--- rails/app/controllers/reviews_controller.rb
+++ rails/app/controllers/reviews_controller.rb
@@ -9,10 +9,12 @@ class ReviewsController < ApplicationController
     @reason_sale = ReasonSale.find(@review.reason_sale)
     @num_sale = NumSale.find(@review.num_sale)
 
+    # rubocop:disable all
     set_meta_tags(
-      title: "#{@company.name}#{@store.name}""の不動産売却体験談を公開中",
-      description: "#{session[:assessment_city]["name"]}" "の不動産売却に強い不動産会社の体験談を紹介します。不動産の価格査定が素早く・簡単にできるのはもちろん、弊社で審査して厳選した、「土地・（中古）マンション・（中古）一戸建て」の売却・買取・査定が得意な、地元のおすすめの加盟不動産会社を比較検討できます。「信頼できる不動産会社を見つけるのが難しい」「自分が知っている不動産会社を信頼してよいかわからない」という方はぜひご活用ください。",
-      keywords: "不動産売却,不動産査定,不動産会社,不動産仲介業者,不動産屋,住宅,住まい,マンション,一戸建て,土地,投資,家売る,ベテランすまい"
+      title: "#{@company.name}#{@store.name}"'の不動産売却体験談を公開中',
+      description: "#{session[:assessment_city]['name']}" 'の不動産売却に強い不動産会社の体験談を紹介します。不動産の価格査定が素早く・簡単にできるのはもちろん、弊社で審査して厳選した、「土地・（中古）マンション・（中古）一戸建て」の売却・買取・査定が得意な、地元のおすすめの加盟不動産会社を比較検討できます。「信頼できる不動産会社を見つけるのが難しい」「自分が知っている不動産会社を信頼してよいかわからない」という方はぜひご活用ください。',
+      keywords: '不動産売却,不動産査定,不動産会社,不動産仲介業者,不動産屋,住宅,住まい,マンション,一戸建て,土地,投資,家売る,ベテランすまい'
     )
+    # rubocop:enable all
   end
 end
--- rails/app/controllers/stores_controller.rb
+++ rails/app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class StoresController < ApplicationController
-  def show    
+  def show
     @store = Store.find(params[:id])
     @company = @store.company
 
@@ -18,10 +18,12 @@ class StoresController < ApplicationController
     end
     @score_total = (@score_contract + @score_speed + @score_store) / 3
 
+    # rubocop:disable all
     set_meta_tags(
-      title: "#{@company.name}#{@store.name}""の不動産売却体験談を公開中",
-      description: "#{session[:assessment_city]["name"]}" "の不動産売却に強い不動産会社の体験談を紹介します。不動産の価格査定が素早く・簡単にできるのはもちろん、弊社で審査して厳選した、「土地・（中古）マンション・（中古）一戸建て」の売却・買取・査定が得意な、地元のおすすめの加盟不動産会社を比較検討できます。「信頼できる不動産会社を見つけるのが難しい」「自分が知っている不動産会社を信頼してよいかわからない」という方はぜひご活用ください。",
-      keywords: "不動産売却,不動産査定,不動産会社,不動産仲介業者,不動産屋,住宅,住まい,マンション,一戸建て,土地,投資,家売る,ベテランすまい"
+      title: "#{@company.name}#{@store.name}"'の不動産売却体験談を公開中',
+      description: "#{session[:assessment_city]['name']}" 'の不動産売却に強い不動産会社の体験談を紹介します。不動産の価格査定が素早く・簡単にできるのはもちろん、弊社で審査して厳選した、「土地・（中古）マンション・（中古）一戸建て」の売却・買取・査定が得意な、地元のおすすめの加盟不動産会社を比較検討できます。「信頼できる不動産会社を見つけるのが難しい」「自分が知っている不動産会社を信頼してよいかわからない」という方はぜひご活用ください。',
+      keywords: '不動産売却,不動産査定,不動産会社,不動産仲介業者,不動産屋,住宅,住まい,マンション,一戸建て,土地,投資,家売る,ベテランすまい'
     )
+    # rubocop:enable all
   end
 end
